c406412c6e8016f6867addbf1f699ed69d078102
hmbadhon2 <hmmizanurrahmanbadhon@gmail.com>
2024-01-10 17:24:25 +0600

    Header add

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../pages/Shared/Header";
 
 const MainLayout = () => {
   return (
     <>
-      <header>header</header>
-      <main>
+      <header>
+        <Header />
+      </header>
+      <main className="w-full max-w-7xl mx-auto">
         <Outlet />
       </main>
       <footer>footer</footer>
---
+++ src/pages/Shared/Header.jsx
@@ -0,0 +1,57 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const headerLink = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="protfolio">Portfolio</Link>
+      </li>
+      <li>
+        <Link to="process">Process</Link>
+      </li>
+      <li>
+        <Link to="case-study">Case Study</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar py-4 max-w-7xl mx-auto px-0">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {headerLink}
+          </ul>
+        </div>
+        <h2 className="primary-color font-bold text-2xl">Animation Web</h2>
+      </div>
+      <div className="navbar-end hidden lg:flex px-0">
+        <ul className="menu menu-horizontal pr-0">{headerLink}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
